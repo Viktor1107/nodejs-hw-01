6d2ab783b6f9ce2c60db5d99913434a28e2afda6
Viktor1107 <162905444+Viktor1107@users.noreply.github.com>
2024-11-21 14:24:41 +0100

    delete console.log(contacts)

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -5,7 +5,6 @@ export const readContacts = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
-    console.log(contacts);
     return contacts || [];
   } catch (error) {
     console.log(error);
